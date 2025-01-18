07b96acfb9e8f4d9e02f76316e74126b610468b4
linyawen <2938543506@qq.com>
2025-01-18 17:00:45 +0800

    feat: 封装store

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>登录</div>
+  <div>hello,{{ userStore.$state.username }}</div>
 </template>
-
+<script lang="ts" setup>
+  import { useUserStore } from "@/store/user";
+  const userStore = useUserStore();
+</script>
 <style scoped lang="scss"></style>
