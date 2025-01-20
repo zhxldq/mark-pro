6fe92c6ef44b95e5f4071c72fd99c8d3d5fce3a3
linyawen <2938543506@qq.com>
2025-01-20 09:08:10 +0800

    feat:eslint

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div>Home</div>
 </template>
-<!-- <script lang="ts" setup></script> -->
+<script lang="ts" setup>
+  import { ref } from "vue";
+
+  const name = ref("lyw");
+</script>
 <style lang="less" scoped></style>
